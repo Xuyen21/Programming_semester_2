d37711b8443f18834330fd8c5016f9e77d7c73c9
Silvan Wiedmer <87522823+SilvanW@users.noreply.github.com>
2023-03-02 13:16:08 +0100

    use of env variables in db connection

--- app/postgres.py
+++ app/postgres.py
@@ -1,10 +1,14 @@
+import os
 import psycopg2
+from dotenv import load_dotenv
+
+load_dotenv()
 
 conn = psycopg2.connect(
     database="Dashboard",
     host="localhost",
-    user="",
-    password="",
+    user=os.getenv('POSTGRES_USER'),
+    password=os.getenv('POSTGRES_PASSWORD'),
     port="5432"
 )
 

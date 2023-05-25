393ded3d91fb4bd0005929c6e84b5f8ef9e48bc2
Silvan Wiedmer <87522823+SilvanW@users.noreply.github.com>
2023-05-25 10:38:30 +0200

    remove "Below" from column description

--- app/modules/column_descriptions.py
+++ app/modules/column_descriptions.py
@@ -1,24 +1,18 @@
 column_description: dict = {
     "author": """
         The "author" column in the DBLP data refers to the name(s) of the author(s) of a particular publication,
-        such as a research paper or a book.\n
-        Below is the visualization of authors with the most publication.""",
+        such as a research paper or a book.""",
     "editor": """
-        The "editor" field in the DBLP database typically refers to the editor(s) of a book or a special issue of a journal.\n
-        Below is the visualization of the number of books edited by each editor.""",
+        The "editor" field in the DBLP database typically refers to the editor(s) of a book or a special issue of a journal.""",
     "school": """The "school" column in the DBLP data refers to the institution or university where the authors of a publication are affiliated with.
-        This information can be useful in visualizing the distribution of research output across different institutions and identifying patterns of collaboration and research partnerships.\n
-        Below is the visualization of the universities/ institutions that has the most publications""",
+        This information can be useful in visualizing the distribution of research output across different institutions and identifying patterns of collaboration and research partnerships.""",
     "pages": """
         The "pages" column in the DBLP data refers to the page numbers of a publication, such as a research paper or a book chapter.
-        Visualizing the "pages" column can provide insights into the length and complexity of the publications in the dataset.\n
-        Below is the visualization of the distribution of page numbers across the publications.
+        Visualizing the "pages" column can provide insights into the length and complexity of the publications in the dataset.
         This could help you identify whether most publications are short or long, or whether there are a few outliers with very long or very short page numbers.""",
     "publisher": """
         The "publisher" column in the DBLP data refers to the publisher of a publication, such as a book or a conference proceeding.
-        Visualizing the "publisher" column can provide insights into the distribution of publications across different publishers and the trends in publishing over time.\n
-        Below is the visualization of the distribution of publications across the most popular publishers
-        """
+        Visualizing the "publisher" column can provide insights into the distribution of publications across different publishers and the trends in publishing over time."""
 }
 def get_column_description(column_name: str = 'author') -> str:
     """

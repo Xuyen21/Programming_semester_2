ab638b561f90e31d4dee7902c3b421ac9e8be11b
Silvan Wiedmer <87522823+SilvanW@users.noreply.github.com>
2023-05-26 21:47:06 +0200

    fix linter issues

--- app/pages/timespan.py
+++ app/pages/timespan.py
@@ -1,33 +1,29 @@
-import os
-from dotenv import load_dotenv
-# load environments from .env file
-load_dotenv()
-
-# not working in VSCODE without that
-if os.getenv('VSCODE') == "True":
-    import sys
-    sys.path.append('./')
-
+"""
+This module is responsible for the timespan tab of the DBLP Dashboard.
+"""
 import plotly.express as px
-
 from dash import dcc, html, Input, Output, Dash
 import dash_bootstrap_components as dbc
 import pandas as pd
 from flask_caching import Cache
 
-from app.components.filter_card import generate_filter_card
-from app.components.settings_card import generate_settings_card
-from app.components.info_card import info_card
+# modules
+from modules.postgres import execute_query
+from modules.postgres_queries import papers_per_month_query, update_year_dropdown_query
 
-from app.modules.postgres import execute_query
-from app.modules.postgres_queries import papers_per_month_query, update_year_dropdown_query
+# components
+from components.filter_card import generate_filter_card
+from components.settings_card import generate_settings_card
+from components.info_card import info_card
 
 # Define form
 timespan_form = html.Div([
     dbc.Row([
         dbc.Label("Table"),
         dcc.Dropdown(
-            options=[{"label": str(year[0]), "value": str(year[0])} for year in execute_query(update_year_dropdown_query(), 'update_year_dropdown')],
+            options=[
+                {"label": str(year[0]), "value": str(year[0])} for year in execute_query(update_year_dropdown_query(), 'update_year_dropdown')
+            ],
             placeholder="Choose the year",
             id="year_dropdown",
             value ="2022"
@@ -75,9 +71,9 @@ def timespan_callback(app: Dash, cache: Cache, cache_timeout: int = 600):
         sql_query = papers_per_month_query(year)
 
         results = execute_query(sql_query, 'papers_per_month')
-        df = pd.DataFrame(results, columns=['month', 'entryType', 'count'])
+        df_papers = pd.DataFrame(results, columns=['month', 'entryType', 'count'])
 
-        return df
+        return df_papers
 
     @app.callback(
         Output("timespan_chart", "figure"),
@@ -89,12 +85,12 @@ def timespan_callback(app: Dash, cache: Cache, cache_timeout: int = 600):
         if not selected_year:
             selected_year = "2022"
 
-        df = papers_per_month(selected_year)
+        df_papers = papers_per_month(selected_year)
 
-        if chart_type == "bar":
-            fig = px.bar(df, x='month', y='count', color='entryType')
-        elif chart_type == "pie":
-            fig = px.pie(df, values='count', names='entryType',color='entryType')
+        if chart_type == "pie":
+            fig = px.pie(df_papers, values='count', names='entryType',color='entryType')
+        else:
+            fig = px.bar(df_papers, x='month', y='count', color='entryType')
 
         fig.update_layout(
             xaxis_title="Month",
